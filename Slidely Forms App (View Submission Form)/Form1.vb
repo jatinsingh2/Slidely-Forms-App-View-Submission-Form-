0aab9a286725ce45df2449c3d404199dc3ed1924
jatin singh <jatinsingh242001@gmail.com>
2024-06-21 12:07:55 +0530

    Add project files.

---
+++ Slidely Forms App (View Submission Form)/Form1.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ViewSubmissionsForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtPhoneNum = New System.Windows.Forms.TextBox()
+        Me.txtGithubLink = New System.Windows.Forms.TextBox()
+        Me.txtStopwatchTime = New System.Windows.Forms.TextBox()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(150, 50)
+        Me.txtName.Name = "txtName"
+        Me.txtName.ReadOnly = True
+        Me.txtName.Size = New System.Drawing.Size(200, 22)
+        Me.txtName.TabIndex = 0
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(150, 100)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.ReadOnly = True
+        Me.txtEmail.Size = New System.Drawing.Size(200, 22)
+        Me.txtEmail.TabIndex = 1
+        '
+        'txtPhoneNum
+        '
+        Me.txtPhoneNum.Location = New System.Drawing.Point(150, 150)
+        Me.txtPhoneNum.Name = "txtPhoneNum"
+        Me.txtPhoneNum.ReadOnly = True
+        Me.txtPhoneNum.Size = New System.Drawing.Size(200, 22)
+        Me.txtPhoneNum.TabIndex = 2
+        '
+        'txtGithubLink
+        '
+        Me.txtGithubLink.Location = New System.Drawing.Point(150, 200)
+        Me.txtGithubLink.Name = "txtGithubLink"
+        Me.txtGithubLink.ReadOnly = True
+        Me.txtGithubLink.Size = New System.Drawing.Size(200, 22)
+        Me.txtGithubLink.TabIndex = 3
+        '
+        'txtStopwatchTime
+        '
+        Me.txtStopwatchTime.Location = New System.Drawing.Point(150, 250)
+        Me.txtStopwatchTime.Name = "txtStopwatchTime"
+        Me.txtStopwatchTime.ReadOnly = True
+        Me.txtStopwatchTime.Size = New System.Drawing.Size(200, 22)
+        Me.txtStopwatchTime.TabIndex = 4
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(50, 300)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(100, 30)
+        Me.btnPrevious.TabIndex = 5
+        Me.btnPrevious.Text = "PREVIOUS (CTRL + P)"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(250, 300)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(100, 30)
+        Me.btnNext.TabIndex = 6
+        Me.btnNext.Text = "NEXT (CTRL + N)"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'ViewSubmissionsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 400)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.txtStopwatchTime)
+        Me.Controls.Add(Me.txtGithubLink)
+        Me.Controls.Add(Me.txtPhoneNum)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtName)
+        Me.Name = "ViewSubmissionsForm"
+        Me.Text = "Jatin, Slidely Task 2 - View Submissions"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+    End Sub
+
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents txtPhoneNum As System.Windows.Forms.TextBox
+    Friend WithEvents txtGithubLink As System.Windows.Forms.TextBox
+    Friend WithEvents txtStopwatchTime As System.Windows.Forms.TextBox
+    Friend WithEvents btnPrevious As System.Windows.Forms.Button
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+End Class
